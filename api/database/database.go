87a2db134b65266c1e59d4dcf76d4f84e3fc63e7
imayrus <imayrus1@gmail.com>
2022-07-20 23:28:57 +0530

    my changes

--- api/database/database.go
+++ api/database/database.go
@@ -2,11 +2,13 @@ package database
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/imayrus/url-shortener/models"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
+	_ "github.com/lib/pq"
 )
 
 var db *gorm.DB
@@ -19,17 +21,23 @@ func DatabaseSetup() {
 		fmt.Println(err)
 	}
 
-	dbconn := "host=172.17.0.2 user=admin password=test dbname=admin port=5432 sslmode=disable"
+	//dbconn := "host=172.17.0.2 user=admin password=test dbname=admin port=5432 sslmode=disable"
 
-	host = os.godotenv("DB_HOST")
-	user = os.godotenv("")
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	//password := os.Getenv("DB_PASS")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	dbdriver := os.Getenv("DB_DRIVER")
 
-	db, err = gorm.Open(postgres.Open(dbconn), &gorm.Config{})
+	dbinfo := fmt.Sprintf("host=%s port=%s user=%s  dbname=%s sslmode=disable", host, port, user,  dbname)
+
+	db, err = gorm.Open(dbdriver, dbinfo)
 	if err != nil {
 		panic(err)
 	}
 
-	err = db.Debug().AutoMigrate(&models.ShortUrl{})
+	db.AutoMigrate(&models.ShortUrl{})
 	if err != nil {
 		fmt.Println(err)
 	}
